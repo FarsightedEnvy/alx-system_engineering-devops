acc48a06a4cb638c00b3c383624ed29ffd31afd2
FarsightedEnvy <keegansaunders49@gmail.com>
2024-06-19 00:29:41 -0700

    bug fixes

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """Function to count all words in all hot posts of a given Reddit subreddit."""
 import requests
 
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """Function to query a list of all hot posts on a given Reddit subreddit"""
 import requests
 
